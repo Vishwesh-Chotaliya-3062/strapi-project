b42b8f56fc07df5218d4b6e19a12f1878cb049f4
Vishwesh32 <v.d.chotaliya30@gmail.com>
2021-09-16 10:33:15 +0530

    Send mail on signup

--- api/strapiusers/controllers/strapiusers.js
+++ api/strapiusers/controllers/strapiusers.js
@@ -1,8 +1,27 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.strapiusers.create(data, { files });
+    } else {
+      entity = await strapi.services.strapiusers.create(ctx.request.body);
+    }
+
+    strapi.services.strapiusers.send('batchmanager619@gmail.com', entity.email, 'Welcome', entity.username);
+
+    ctx.send({
+        ok: true
+    })
+
+    return sanitizeEntity(entity, { model: strapi.models.strapiusers });
+  },
+};
--- api/strapiusers/services/strapiusers.js
+++ api/strapiusers/services/strapiusers.js
@@ -5,4 +5,28 @@
  * to customize this service
  */
 
-module.exports = {};
+ const nodemailer = require('nodemailer');
+
+ // Create reusable transporter object using SMTP transport.
+ const transporter = nodemailer.createTransport({
+   service: 'Gmail',
+   auth: {
+     user: 'batchmanager619@gmail.com',
+     pass: 'Vronaldo@619',
+   },
+ });
+ 
+ module.exports = {
+   send: (from, to, subject, text) => {
+     // Setup e-mail data.
+     const options = {
+       from,
+       to,
+       subject,
+       text,
+     };
+ 
+     // Return a promise of the function that sends the email.
+     return transporter.sendMail(options);
+   },
+ };
